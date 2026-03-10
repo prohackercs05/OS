945f70720b0d431f2cabd1c624ba690e4c2c2768
prohackercs05 <prohackercs03@gmail.com>
2026-03-10 16:57:02 +0530

    Reorganized OS slips folders (s1-s30)

---
+++ s3/p1.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+
+int main() {
+    int n=5, r=4;
+    int alloc[5][4] = {{0,0,1,2}, {1,0,0,0}, {1,3,5,4}, {0,6,3,2}, {0,0,1,4}};
+    int max[5][4] = {{0,0,1,2}, {1,7,5,0}, {2,3,5,6}, {0,6,5,2}, {0,6,5,6}};
+    int avail[4] = {1,5,2,0};
+    int need[5][4], i, j, k;
+
+    
+    printf("Need Matrix:\n");
+    for(i=0; i<n; i++) {
+        for(j=0; j<r; j++) {
+            need[i][j] = max[i][j] - alloc[i][j];
+            printf("%d ", need[i][j]);
+        }
+        printf("\n");
+    }
+
+    int finish[5] = {0}, safeSeq[5], ind = 0;
+    for (k = 0; k < 5; k++) {
+        for (i = 0; i < n; i++) {
+            if (finish[i] == 0) {
+                int flag = 0;
+                for (j = 0; j < r; j++) {
+                    if (need[i][j] > avail[j]) {
+                        flag = 1;
+                        break;
+                    }
+                }
+                if (flag == 0) {
+                    safeSeq[ind++] = i;
+                    for (int y = 0; y < r; y++) avail[y] += alloc[i][y];
+                    finish[i] = 1;
+                }
+            }
+        }
+    }
+
+    int flag = 1;
+    for(i=0; i<n; i++) if(finish[i]==0) flag=0;
+
+    if(flag==1) {
+        printf("System is in Safe State.\nSafe Sequence: ");
+        for (i = 0; i < n - 1; i++) printf("P%d -> ", safeSeq[i]);
+        printf("P%d\n", safeSeq[n - 1]);
+    } else {
+        printf("System is NOT in Safe State!\n");
+    }
+
+    return 0;
+}
---
+++ s9/p2.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void sort(int arr[], int n) {
+    for (int i = 0; i < n - 1; i++)
+        for (int j = 0; j < n - i - 1; j++)
+            if (arr[j] > arr[j + 1]) {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+}
+
+int main() {
+    int n, head, i, total=0;
+    printf("Enter num requests and head: ");
+    scanf("%d %d", &n, &head);
+    int req[n+1], pos;
+    printf("Enter requests: ");
+    for(i=0; i<n; i++) scanf("%d", &req[i]);
+    
+    req[n]=head;
+    sort(req, n+1);
+    for(i=0; i<n+1; i++) if(req[i]==head) { pos=i; break; }
+
+    printf("Order: ");
+    
+    for(i=pos-1; i>=0; i--) {
+        printf("%d ", req[i]);
+        total += abs(req[i]-head);
+        head = req[i];
+    }
+    for(i=pos+1; i<n+1; i++) {
+        printf("%d ", req[i]);
+        total += abs(req[i]-head);
+        head = req[i];
+    }
+    printf("\nTotal movement: %d\n", total);
+    return 0;
+}
